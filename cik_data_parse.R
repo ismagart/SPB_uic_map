723e77d44d2e7513f7778a211a27acd67a7d6b50
Artem Ismagilov <art.ismagilov@gmail.com>
2018-12-26 18:35:05 +0300

    Write parse from cik site, find possibility to create wrapper around site and create some kind of API. Add result of election to csv

--- cik_data_parse.R
+++ cik_data_parse.R
@@ -4,6 +4,7 @@ library(rvest)
 library(dplyr)
 library(xml2)
 library(rlang)
+library(tidyr)
 # необходимо сгрузить данные по выборам с сайта цик
 
 # важно помнить, что некоторые УИК является временными, а значит в uic_table их нет
@@ -15,213 +16,169 @@ library(rlang)
 # Selector gadget показал, что на сайте нет css и все надо через xpath вытаскивать
 spb_election_url <- "http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&root=1000075&tvd=100100084849201&vrn=100100084849062&region=78&global=1&sub_region=78&prver=0&pronetvd=null&vibid=100100084849201&type=227"
 
-web <- GET(spb_election_url)
-
-# table <- html_nodes(content(web), "table")
-
-pre_table <- html_nodes(content(web), "table")[3]
-
-test_table <- html_nodes(pre_table, "table")
-# суммированя таблица по всем тикам
-summary_pre_table <- test_table[7]
-# ВАЖНО, что модификация summary_pre_table влияет на оригинал
-# не ясно пока как изолировать их. вся проблема с особенностях xml как такового и семантики R
-# XML2 магия, убираем тег br и заменяем его 
-# ищем все теги br (на выходе получаем вектор)
-summary_pre_table %>%  
-  xml_find_all(".//br") %>% 
-  # добавляем новый тег <p> со значением \n
-  xml_add_sibling("p", "\n")
-
-summary_pre_table %>%  
-  xml_find_all(".//br") %>% 
-  xml_remove()
-
-# Значения абсолютные и процентные разделены пробелом
-summary_info <- html_table(summary_pre_table, fill = TRUE, trim = TRUE)
-View(summary_info)
-
-# сами резульаты по тикам
-tic_pre_table <- test_table[8]
-
-tic_pre_table %>%  
-  xml_find_all(".//br") %>% 
-  # добавляем новый тег <p> со значением \n
-  xml_add_sibling("p", "\n")
-
-tic_pre_table %>%  
-  xml_find_all(".//br") %>% 
-  xml_remove()
-
-tic_result <- html_table(tic_pre_table, fill = T)
-View(tic_result)
-# intermediate_obj <- test_table[7]
-# summary_pre_table <- intermediate_obj
-tracemem(test_table[7])
-
-# всё равно меняется
-summary_pre_table <- rlang::duplicate(test_table[7], shallow = FALSE)
-tracemem(summary_pre_table)
-
-# Показывает разные ячеки памяти, но
-mody_summary_pre_table <- test_table[7]
-tracemem(mody_summary_pre_table)
-str(mody_summary_pre_table)
-mody_summary_pre_table[[1]]$mod <- "0"
-mody_summary_pre_table[[1]]$mod <- NULL
-xml_name(mody_summary_pre_table[1])
-tracemem(mody_summary_pre_table[1])
-
-xml_name(mody_summary_pre_table)
-xml_set_name(mody_summary_pre_table, "new table")
-
-
-gg <- mody_summary_pre_table[[1]]
-tracemem(gg)
-xml_set_name(gg, "new table")
-gg
-
-xml_name(test_table[7])
-
-# через лист
-gg <- pairlist(test_table[7])
-gg[[1]]
-ggt <- list(test_table[7])
-xml_name(gg[[1]])
-xml_name(test_table[7])
-
-xml2::xml_name(gg[[1]]) <- "new table"
-
-# Попробуем поменять имя
-# всё равно меняются другие файлы 
-xml2::xml_name(summary_pre_table) <- "new table"
-xml_name(summary_pre_table)
-
-xml_name(test_table[7])
-xml_name(test_table[7]) <- "table"
-xml_name(test_table[7])
-xml_name(summary_pre_table)
-
-
-
-# через функцию
-new_name_xml <- function(input_xml, new_name){
-  xml_set_name(input_xml, new_name)
-  return(input_xml)
+# Функция по модификации таблиц, так как на сайте результаты в абсолютных значениях
+# и процентах находятся в одной ячеке и разедели тегом br, то надо модицифировать
+# ибо html_table сливает их вместе
+ht_table_modifer <- function(input_table){
+  input_table %>%  
+    xml2::xml_find_all(".//br") %>% 
+    # добавляем новый тег <p> со значением \n
+    xml2::xml_add_sibling("p", "\n")
+  
+  input_table %>%  
+    xml2::xml_find_all(".//br") %>% 
+    xml2::xml_remove()
+  return(input_table)
 }
 
-summary_pre_table <- new_name_xml(summary_pre_table, "new table")
-xml_name(summary_pre_table)
-xml_name(test_table[7])
+# Основная функция, по ссылке на страницу с таблицей выстаскивает её приводится к
+# длинному виду и приводит столбцы к нужному формату
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Имеет смысл переписать её чтоб внутри работать с матрицами ибо это будет быстрее
+parse_tic_data <- function(link_to_tic_data, percent_value = NULL){
+  web_page <- httr::GET(link_to_tic_data)
+  table_xpath <- "//table[descendant::table[@align]]"
+  extract_content <- rvest::html_nodes(content(web_page), xpath = table_xpath)
+  # берем именно второй, так как в первом содержится таблица всей страницы
+  pre_table <- rvest::html_nodes(extract_content[2], "table")
+  # необходимо модифицировать их, чтобы разделить абсолютные и проценты
+  mod_table <- lapply(pre_table, function(x) html_table(ht_table_modifer(x), fill = TRUE))
+  
+  # собираем один дата фрейм попутно убираем первую колонку
+  result_table <- cbind(mod_table[[1]][,-1], mod_table[[2]])
+  # делаем нормальные имена
+  # необходимо убрать символ № так как с ним все плохо, кодируется как \271
+  # нужно унифицировать чтоб годилось как для ТИК так и для УИК
+  # или оставить как есть, ибо если расширять её на россию, то там есть и просто название городов
+  # заменить только пробелы на нижнее подчеркивание
+  res_column_name <- stringr::str_remove(result_table[1,], "№") %>% 
+    stringr::str_replace_all(.,"[ ]", "_")
+  # знаем, что первый две колонки содержат информацию о бюллетенях
+  # колонка sum в абсолютных значениях описвыает сумму голосов за кандидата на этом территориальном избирательном участке
+  res_column_name[1:2] <- c("title", "sum")
+  names(result_table) <- res_column_name
+  result_table <- result_table[-1,] %>% 
+    # убираем пустую строчку между блоком кандидатов и бюллетенями
+    na.omit(.)
+  # имеет смысл разбить таблицу на две 
+  # 1) вся инфа о бюллетенях
+  # 2) именно кандидаты + можно тут выбрать абсолютные значения или проценты
+  # Разделям за счет того, что во всех строчках про бюллетени есть фраза число
+  indicate <- stringr::str_detect(result_table$title, "^Число")
+  election_result <- split(result_table, f = indicate )
+  names(election_result) <- c("candidate", "bulletin_info")
+  # приводим к длинному виду
+  # цикл по индексам в листе данным
+  lapply(1:2, function(x){
+    # так как мы знаем какие два столбца мы сами создали, то передаем их имена явно черех -
+    election_result[[x]] <<- gather(election_result[[x]], key = IC, value = result, -sum, -title, factor_key = TRUE)
+  })
+  # приводим к числовому формату, заголовок title оставляем как character
+  election_result$bulletin_info <- dplyr::mutate_at(election_result$bulletin_info, funs(as.numeric(.)), .vars = c("result", "sum"))
+  # приводим к формату данные и разделяем проценты
+  if(is.null(percent_value) == FALSE ){
+    maneuver_data <- election_result$candidate
+    election_result$candidate <- NULL
+    # огромная простыня
+    election_result$candidate$absolute <- dplyr::mutate_at(maneuver_data, 
+                                                           funs(as.numeric(stringr::str_remove_all(.,"[\\n][[:digit:][:punct:]]*%"))), .vars = c("result", "sum")) %>% 
+      mutate(., title = as.factor(title))
+    # через группировки и str_replace е получается, по-видимому вложенные группы не распознаются
+    election_result$candidate$percentage <- mutate_at(maneuver_data,
+                                                      funs(as.numeric(stringr::str_remove_all(stringr::str_extract(.,"[\\n][[:digit:][:punct:]]*%"), "[^[:digit:].,]"))), .vars = c("result", "sum")) %>%
+      mutate(., title = as.factor(title))
+  }else{
+    election_result$candidate <- dplyr::mutate_at(election_result$candidate, 
+                                                  funs(as.numeric(stringr::str_remove_all(.,"[\\n][[:digit:][:punct:]]*%"))), .vars = c("result", "sum")) %>% 
+      mutate(., title = as.factor(title))
+  }
+  # возвращаем лист, внутри которого дата-фреймы
+  return(election_result)
+}
 
-x <- read_xml("<table><b/></table>")
-x
-xml_name(x)
+# %%%%%%%%%%%Вытаскиваем с со сводной таблицы по спб ссылки на ТИК %%%%%%%%%%%
+# попробуем прописать через Xpath нужные там ссылки чтобы не опираться только на цифры
+# есть проблемы с русским языком всё дело в кодировках, пора все же уходить на UNIX
+# rvest поддерживает только xpath 1.0 
+xpath_query <- "//a[contains(text(),'Территориальная')]"
+# Вручную задаём кодировка для запроса.
+# перед этим проверили какая кодировка у сайта
+
+# нужно считать сводную таблицу по спб
+web <- GET(spb_election_url)
+link_to_tic_path <- html_nodes(content(web), xpath = enc2utf8(xpath_query))
+# получаем вектор ссылок
+# Эти ссылки будут работать только для СПб, так как ищем по названиям ТИК-а
+link_to_tic <- html_attr(link_to_tic_path, "href")
+# Так же берем имена, чтобы лист был именован
+link_name <- html_text(link_to_tic_path)
+# Так как работает с питером, то имеет смысл просто называть TIC 
+link_name <- stringr::str_remove(link_name, "\271") %>% 
+  stringr::str_extract(.,"[[:digit:]]+") %>% 
+  paste0("TIC_", .)
 
 
-xml_name(test_table[7])
-xml_name(summary_pre_table)  
+spb_election_list <- list()
+# Возможно имеет смысл сдеть задержку, мало ли могут банить
+spb_election_list <- lapply(1:length(link_name), function(x){
+  parse_tic_data(link_to_tic[x])
+} )
+names(spb_election_list) <- link_name
+
+# Так жe можно скачать данные по Тика
+spb_general_election <- parse_tic_data(spb_election_url)
 
 
+# Удобнее работать с уже сведенными данными, для этого
+# объединяем все Тик результаты по кандидатам в один дата фрей
+# Будет работать пока нет процентов
+temp_table <- lapply(spb_election_list, function(x) x$candidate)
+library(plyr)
+spb_common_election <- plyr::ldply(temp_table, data.frame) %>% 
+  mutate(.id = as.factor(.id)) %>% 
+# приходится явно писать пакет ибо есть конфликты с plyr
+  dplyr::rename(ter_ic = .id)
 
-xml_name(test_table[7])
-xml_name(summary_pre_table)
-
-# возвращаем имя назад
-xml_name(summary_pre_table) <- "table"
-
-xml_name(summary_pre_table)
-xml_name(test_table[7])
+View(spb_common_election)
+str(spb_common_election)
+# Сохраняем для локального использования
+saveRDS(spb_common_election, "spb_president_election.RDS")
+# сохраняем в csv для выгрузки
+write.csv(spb_common_election, "spb_president_election.csv")
+# test_common_dt <- unlist(test, use.names = TRUE, recursive = FALSE)
  
+# для удобства имеет смысл все данные по УИК объеденить в один дата фрейм,
+# где будет графа принадлежнести к определенному тику.
+# однако так же будет разграничение на бюлетени и данные с места
 
-# Проверяем изменился ли первоначальный файл
- # и да он меняется
- write_xml(test_table[7], "./Election_analysis/summary_dirty_table_after.xml")
- 
-# удаляем этот тег br
-# хорошо бы посмотреть на tree view теперь
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# О возможности сделать своего рода api для выгрузки данных с сайта цик
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+library(httr)
+library(rvest)
+# url_cik <- "http://www.vybory.izbirkom.ru/region/izbirkom"
+url_cik <- "http://www.izbirkom.ru/region/izbirkom"
+list_qury <- list(start_date = "01.01.2019",
+                  urovproved = "all",
+                  vidvibref = "all",
+                  vibtype = "all",
+                  end_date = "30.09.2019",
+                  actual_regions_subjcode = "5",
+                  old_regions_subjcode = "41^^59^^75^^80^^81^^82^^84^^88",
+                  sxemavib = "all",
+                  action = "search_by_calendar",
+                  region = "0",
+                  ok = enc2utf8("Искать"))
+# actual_regions_subjcode = "0^^1^^2^^3^^4^^5^^6^^7^^8^^9^^10^^11^^93^^12^^13^^14^^15^^16^^17^^18^^19^^20^^21^^22^^92^^91^^23^^24^^90^^25^^26^^27^^28^^29^^30^^31^^32^^33^^34^^35^^36^^37^^38^^39^^40^^42^^43^^44^^45^^46^^47^^48^^49^^50^^51^^52^^53^^54^^55^^56^^57^^58^^60^^61^^62^^63^^64^^65^^66^^67^^68^^69^^70^^71^^72^^73^^74^^76^^77^^78^^94^^79^^83^^85^^86^^87^^89"
+# old_regions_subjcode = "41^^59^^75^^80^^81^^82^^84^^88"
+# request <- "start_date=18.03.2018&urovproved=all&vidvibref=all&vibtype=all&end_date=18.03.2018&actual_regions_subjcode=0^^1^^2^^3^^4^^5^^6^^7^^8^^9^^10^^11^^93^^12^^13^^14^^15^^16^^17^^18^^19^^20^^21^^22^^92^^91^^23^^24^^90^^25^^26^^27^^28^^29^^30^^31^^32^^33^^34^^35^^36^^37^^38^^39^^40^^42^^43^^44^^45^^46^^47^^48^^49^^50^^51^^52^^53^^54^^55^^56^^57^^58^^60^^61^^62^^63^^64^^65^^66^^67^^68^^69^^70^^71^^72^^73^^74^^76^^77^^78^^94^^79^^83^^85^^86^^87^^89&old_regions_subjcode=41^^59^^75^^80^^81^^82^^84^^88&sxemavib=all&action=search_by_calendar&region=0&ok=Искать"
+# Всё заработало, надо лишь правильно encode выставить
+test_post <- httr::POST(url_cik, body = list_qury, encode = "form")
+test <- content(test_post, as = "text")
 
-write_xml(summary_pre_table, "./Election_analysis/summary_pre_table.xml")
+write(test, "cik_post.txt")
+str(test)
+gg <- content(test_post, "parsed")
+test_table <- html_nodes(gg, "table")
+test_table[[8]]
 
-
-gg <- xml_find_all(summary_pre_table, ".//br")
-gg <- xml_add_sibling(gg, "p", "\n")
-gg <- xml_find_all(summary_pre_table, ".//br")
-gg <- xml_remove(gg)
-ggt <- html_table(gg)
-
-gg[[1]]
-# Почему-то действия пайп-лайна распространяются и на другие элементы
-
-# clean_sum_table <- xml_replace(summary_pre_table, ".//br", "\n")
-
-clean_td <- html_table(summary_pre_table, fill = TRUE, trim = TRUE)
-
-View(clean_td)
-x <- read_xml("<parent><child>1</child><child>2<child>3</child></child></parent>")
-children <- xml_children(x)
-t1 <- children[[1]]
-t2 <- children[[2]]
-t3 <- xml_children(children[[2]])[[1]]
-x
-# здесь функции именно меняют аргументы, но как они узнают окружение??
-xml_add_sibling(t1, t3)
-xx
-x
-# если вызвать ещё
-xml_add_sibling(t1, t3)
-x
-xml_add_sibling(t3, t1, where = "before")
-x
-# Проблема, на сайте каждая ячейка имеет два числа абсолётное и процент
-# при парсинге они слипаются всё из-за тега br`
-
-
-# внутри есть куча вложенных таблиц
-# result_table<-html_table(table[[9]],fill=TRUE)
-
-
-
-link_to_tic <- html_nodes(content(web), "a")
-link_to_tic[1]
-# с 9 значения идут ссылки на сводные таблицы по ТИК
-link_to_tic[[9]]
-link_to_tic[[38]]
-
-# Все ссылки на вложенные таблицы делаются вручную, как автоматизировать чтоб
-# сделать api е ясно. 
-# Надежда что порядок "вложенности" сохраняется
-
-x <- read_xml("<a><b/></a>")
-y <- read_xml("<a><b/></a>")
-z <- duplicate(x, shallow = FALSE)
-
-tracemem(x)
-tracemem(z)
-str(z)
-z$mod <- 0
-tracemem(z)
-x
-z
-
-
-xml_set_name(x, "new1")
-x
-y
-z
-
-x1 <- c(1, 2, 3)
-x2 <- x1
-tracemem(x1)
-tracemem(x2)
-
-xml_set_name(x, "new3")
-# делаем новое окружение
-e1 <- new.env()
-e1$x <- read_xml("<a><b/></a>")
-e1[["x"]]
-e2 <- new.env()
-e2$z <- duplicate(e1[["x"]])
-e2$z
-
-xml_set_name(e1$x, "new1")
